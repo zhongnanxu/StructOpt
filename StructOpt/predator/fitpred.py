7c34ef2a00a4c334a6ca6d40aaa478ac801562bd
jason <jjmaldonis@gmail.com>
2016-04-05 16:38:19 -0500

    cleaned up predator code

--- StructOpt/predator/fitpred.py
+++ StructOpt/predator/fitpred.py
@@ -1,7 +1,8 @@
 from StructOpt.tools import get_best
 
 def fitpred(pop,Optimizer):
-    """Predator function to select best structures
-    """
-    pop = get_best(pop,Optimizer.nindiv)
+    """Predator function to select best structures"""
+
+    pop = get_best(pop, Optimizer.nindiv)
+
     return pop, ''
